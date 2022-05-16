8df26d9a5d3a0d00b0e7f88f4d045842380c8ebc
Edson Pedro da Silva Sales <edson.sales@gpqueiroz.corp>
2022-05-16 15:31:01 -0400

    detalhes da aplicaçãoo

--- lista/views.py
+++ lista/views.py
@@ -13,11 +13,11 @@ def index(request):
     dataHoje = str(date.today())
     # data = calendar.TextCalendar(calendar.SUNDAY)
 
-    dia_semana = calendar.weekday(ano, mes, dia) #atualizar na sexta dia 4  //  #o dia da semana retorna.  seg==0,ter==1,qua==2,qui==3,sex==4, sab==5,DO==6
+    dia_semana = calendar.weekday(ano, mes, dia) #atualizar na sexta / dia da semana retorna seg==0,ter==1,qua==2,qui==3,sex==4, sab==5,DO==6
     
     nomes = np.loadtxt(fname = "nomes.txt", dtype=str)
-    
-    if dia_semana == 4 and nomes[0] != dataHoje: ## nome[0] quarda a data ta da ultima atualização que precisa ser diferente para atualizar uma vez no dia escolhido apos isso a data passa á ser igual impedindo atualizar por uma semana
+    # se for sexta e a data da ultima atualizaçã 'nome[0] for diferente de hoje'
+    if dia_semana == 4 and nomes[0] != dataHoje: ## nome[0] quarda a data da ultima atualização //data precisa ser diferente para atualizar uma vez no dia escolhido -> 'Sexta' // apos isso a data passa á ser igual impedindo atualizar ate a proxima sexta.
         n = nomes[6]
         nomes = np.delete(nomes,(6),axis=0)
         nomes = np.insert(nomes, 1, n)
